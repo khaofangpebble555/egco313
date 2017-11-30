d112264b459e772f6ea5e18f09b0afe0cea22adc
khaofangpebble555 <31722802+khaofangpebble555@users.noreply.github.com>
2017-11-30 16:59:04 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php $link = mysql_connect('localhost', 'root'); ?>
+<html>
+<head>
+        <title>Hello world!</title>
+        <style>
+        body {
+                background-color: white;
+                text-align: center;
+                padding: 50px;
+                font-family: "Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
+        }
+
+        #logo {
+                margin-bottom: 40px;
+        }
+        </style>
+</head>
+<body>
+        <img id="logo" src="https://www.thesun.co.uk/wp-content/uploads/2016/07/nintchdbpict000193574968.jpg?w=960" />
+        <h1><?php echo "Hello world!"; ?></h1>
+        <?php if(!$link) { ?>
+                <h2>Can't connect to local MySQL Server!</h2>
+        <?php } else { ?>
+                <h2>MySQL Server version: <?php echo mysql_get_server_info(); ?></h2>
+        <?php } ?>
+</body>
+</html>
+
+
+
+
